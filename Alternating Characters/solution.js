345e3d5a5c570fecd2ab513ff5e00b42425a8808
Rohit Anand <rohitanand3456@gmail.com>
2020-07-09 10:40:04 +0530

    added altering character problem and solution in JS

---
+++ Alternating Characters/solution.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the alternatingCharacters function below.
+function alternatingCharacters(s) {
+    let arr_str = s.split('');
+    let i =0 ;
+
+    while(i!==arr_str.length-1){
+        console.log(arr_str.length);
+        if(arr_str[i]===arr_str[i+1]){
+            arr_str.splice((i+1),1,);
+        } else {
+            i=i+1;
+        }
+    }
+
+    return s.length-arr_str.length;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        let result = alternatingCharacters(s);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
